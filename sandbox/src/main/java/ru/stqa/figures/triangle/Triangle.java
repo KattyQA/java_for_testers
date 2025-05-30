d29a3461329925d84d551b04a064370c260a9dd8
Ekaterina Ponomareva <katyushkaashihmina@yandex.ru>
2025-05-30 14:00:37 +0500

    Добавлен класс Triangle, тесты для проверки функций периметра и площади треугольника

---
+++ sandbox/src/main/java/ru/stqa/figures/triangle/Triangle.java
@@ -0,0 +1,24 @@
+package ru.stqa.figures.triangle;
+
+public class Triangle {
+    private double a;
+    private double b;
+    private double c;
+
+    public Triangle(double a, double b, double c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public double perimeter(Triangle triangle){
+        return this.a + this.b + this.c;
+    }
+
+    public double area(Triangle triangle){
+        double p2 = (triangle.perimeter(triangle))/2;
+        return Math.sqrt(p2*(p2 - this.a)*(p2 - this.b)*(p2 - this.c));
+    }
+
+
+}
---
+++ sandbox/src/test/java/ru/stqa/figures/triangle/TriangleTests.java
@@ -0,0 +1,19 @@
+package ru.stqa.figures.triangle;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TriangleTests {
+
+    @Test
+    void getTrianglePerimeter(){
+        Triangle triangle = new Triangle(2.0, 2.0, 3.0);
+        Assertions.assertEquals(7.0, triangle.perimeter(triangle));
+    }
+
+    @Test
+    void  getTriangleArea(){
+        Triangle triangle = new Triangle(23.0, 24.0, 12.0);
+        Assertions.assertEquals(135.85263155345942, triangle.area(triangle));
+    }
+}
